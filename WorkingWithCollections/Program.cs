53b86d51b22929434eac689f92d7676375b6f5e0
theolamide <ajadidan@gmail.com>
2020-10-15 23:51:24 -0400

    Add project files.

---
+++ WorkingWithCollections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WorkingWithCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "OldsMobile";
+            car1.Model = "Cutlas Supreme";
+
+            Car car2 = new Car();
+            car2.Make = "Geo";
+            car2.Model = "Prism";
+
+            Book b1 = new Book();
+            b1.Author = "Robert Tabor";
+            b1.Title = "This life na one o";
+            b1.ISBN = "0-000-0000-000";
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string  Model { get; set; }
+
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
